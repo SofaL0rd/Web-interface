88a908f3b9a322cf3a5e9b78519ba1fae37e79f5
SofaL0rd <nazzar2303@gmail.com>
2024-03-12 22:41:40 +0200

    Added Services, Repositpry,Models. Changed WeatherForecastController

--- api/Controllers/WeatherForecastController.cs
+++ api/Controllers/WeatherForecastController.cs
@@ -1,9 +1,11 @@
+using api.Models;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace api.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -12,6 +14,8 @@ namespace api.Controllers
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private static List<WeatherForecast> forecasts = new List<WeatherForecast>(); 
+
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
@@ -21,13 +25,37 @@ namespace api.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return forecasts;
+        }
+
+        [HttpPost]
+        public IActionResult Post(WeatherForecast weatherForecast)
+        {
+
+            forecasts.Add(weatherForecast);
+            return Ok();
+        }
+
+        [HttpPost("AutoCreate")]
+        public IActionResult Post([FromBody] int count)
+        {
+            if (count <= 0)
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                return BadRequest("Count must be greater than zero.");
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                var newForecast = new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(Random.Shared.Next(1, 10))),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                };
+                forecasts.Add(newForecast);
+            }
+
+            return Ok();
         }
     }
 }
